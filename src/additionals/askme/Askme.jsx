f1f0fa111bb96c3dc445e7258b989face32d5701
Sourav Lahon <lahoncs@gmail.com>
2024-12-05 16:47:33 +0530

    removed bubbles

--- src/additionals/askme/Askme.jsx
+++ src/additionals/askme/Askme.jsx
@@ -4,38 +4,7 @@ import AskmeHelper from './AskmeHelper';
 const Home = () => {
   return (
     <>
-      <div id="background-wrap">
-        <div class="bubble x1">
-          <h1>💕</h1>
-        </div>
-        <div class="bubble x2">
-          <h1>❤️</h1>
-        </div>
-        <div class="bubble x3">
-          <h1>💞</h1>
-        </div>
-        <div class="bubble x4">
-          <h1>💝</h1>
-        </div>
-        <div class="bubble x5">
-          <h1>💖</h1>
-        </div>
-        <div class="bubble x6">
-          <h1>💓</h1>
-        </div>
-        <div class="bubble x7">
-          <h1>💗</h1>
-        </div>
-        <div class="bubble x8">
-          <h1>💘</h1>
-        </div>
-        <div class="bubble x9">
-          <h1>❤️‍🔥</h1>
-        </div>
-        <div class="bubble x10">
-          <h1>❣️</h1>
-        </div>
-      </div>
+
       <AskmeHelper />
     </>
 
